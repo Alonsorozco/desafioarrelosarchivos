7bda8fdf2ba7b1b340bad9ee3932813322d4f4f3
alonzorozco <sfabian.orozco@gmail.com>
2021-05-28 21:31:05 -0400

    add:graficos 2.0

--- calculo_notas2.rb
+++ calculo_notas2.rb
@@ -1,12 +1,40 @@
-data = File.open('notas.data').readlines
-data.map!{|line| line.split(",")}
+input = File.open('notas.data').readlines
 
-def nota_mas_alta(grades)
+input.map! do |i|
+    i.chomp.split(",")
+end 
+def nota_mas_alta(array)
     results = []
-    grades.each do |student_max_grade|
-        results.push([student_max_grade[0], student_max_grade.map {|num| num.to_i}.max])
+    array.each do |x|
+        nombre = [x[0],x.map{|num| num.to_i}.max]
+        results.push (nombre)
     end
     return results
 end
 
-pp nota_mas_alta(data)
+print nota_mas_alta(input)
+print "\n"
+
+
+
+
+
+
+
+
+
+
+
+
+# data = File.open('notas.data').readlines
+# data.map!{|line| line.split(",")}
+
+# def nota_mas_alta(grades)
+#     results = []
+#     grades.each do |student_max_grade|
+#         results.push([student_max_grade[0], student_max_grade.map {|num| num.to_i}.max])
+#     end
+#     return results
+# end
+
+# pp nota_mas_alta(data)
