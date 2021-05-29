edf35300799947cfde0922f6187652deefb56031
alonzorozco <sfabian.orozco@gmail.com>
2021-05-28 21:40:21 -0400

    add:calculo de notas

--- calculo_notas2.rb
+++ calculo_notas2.rb
@@ -15,26 +15,3 @@ end
 print nota_mas_alta(input)
 print "\n"
 
-
-
-
-
-
-
-
-
-
-
-
-# data = File.open('notas.data').readlines
-# data.map!{|line| line.split(",")}
-
-# def nota_mas_alta(grades)
-#     results = []
-#     grades.each do |student_max_grade|
-#         results.push([student_max_grade[0], student_max_grade.map {|num| num.to_i}.max])
-#     end
-#     return results
-# end
-
-# pp nota_mas_alta(data)
