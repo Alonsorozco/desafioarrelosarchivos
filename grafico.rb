a9c505e5515dd97d5674b5b6223ed77bf93dddd2
alonzorozco <sfabian.orozco@gmail.com>
2021-05-28 20:43:57 -0400

    add.grafico 2.0

--- grafico.rb
+++ grafico.rb
@@ -1,11 +1,15 @@
 data = [5, 3, 2, 5, 10]
 
-def chart (arr)
-    max = arr.max
-    arr.each { |num| puts "|" + "**" * num}
-    print ">" + "--" * max
+def chart (array)
+    maximo = array.max
+    array.each do |i|
+        puts "|" + "**" *i
+    end 
+    print ">" + "--" * maximo
     print "\n"
-    (1..max).each { |n| print "#{n} " }
+    (1..maximo).each do |n|
+        print "#{n} " 
+    end
+    print "\n"
 end
-
 chart (data)
